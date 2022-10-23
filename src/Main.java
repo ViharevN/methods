ba5280289ce3ed34aba3035fc0d33a2cc5ca1128
ViharevN <swix_off@mail.ru>
2022-10-23 16:31:42 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        //задание 1
 
+        int currentYear = leapYear(2022);
+    }
+
+    public static int leapYear(int currentYear) {
+        if (currentYear % 4 == 0 && currentYear % 100 != 0 && currentYear % 400 != 0) {
+            System.out.println(currentYear + " год високосный");
+        }
+        else {
+            System.out.println(currentYear +" год не високосный");
+        }
+        return currentYear;
     }
 }
